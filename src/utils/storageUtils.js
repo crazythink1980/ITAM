d889da0545c289fffe4b4e4bd752c3345409659d
crazythink1980 <crazythink@gmail.com>
2021-06-22 16:10:15 +0800

    解决default import/no-anonymous-default-export告警

--- src/utils/storageUtils.js
+++ src/utils/storageUtils.js
@@ -2,6 +2,7 @@ import store from 'store'
 
 const USER_KEY = 'user_key'
 
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
     saveUser(user) {
         store.set(USER_KEY, user)
